13ea32825db36cca808315feb3d14680547e4be6
jmertz97 <32798648+jmertz97@users.noreply.github.com>
2024-09-06 01:18:30 -0400

    Create NdfScriptableStringList.cs
    Based on the structure of NdfScriptableClassList.cs. Enables looking up TableString by value.
    
    Example usage in .py script:
    Classes["TUniteAuSolDescriptor"][17694]["MotherCountry"] = Strings["US"]

---
+++ moddingSuite/Model/Ndfbin/Types/AllTypes/NdfScriptableStringList.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace moddingSuite.Model.Ndfbin.Types.AllTypes
+{
+    public class NdfScriptableStringList
+    {
+        private readonly IList<NdfStringReference> Strings;
+
+        public NdfScriptableStringList(IList<NdfStringReference> strings)
+        {
+            Strings = strings;
+        }
+
+        /// <summary>
+        /// Easy string lookup by value for scripts.
+        /// </summary>
+        /// <returns></returns>
+        public NdfString this[string value] => new NdfString(Strings.FirstOrDefault(cls => cls.Value == value));
+    }
+}
